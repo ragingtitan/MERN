7e2c4669e6b1602899d25b67e49ae505a5ea587d
Anish Das <135655078+ragingtitan@users.noreply.github.com>
2024-02-25 19:12:22 +0000

    installed vite with react

--- first-app/src/App.jsx
+++ first-app/src/App.jsx
@@ -1,13 +1,23 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import './components/Footer.css'
 import './App.css'
-
+import Card from './components/Card'
 function App() {
 
   return (
     <>
-      
+      <Navbar/>
+      <div className="cards">
+        <Card title="card1" description="Card 1 desc" />
+        <Card title="card2" description="Card 2 desc"/>
+        <Card title="card3" description="Card 2 desc"/>
+        <Card title="card4" description="Card 4 desc"/>
+      </div>
+      <Footer/>
     </>
   )
 }
---
+++ first-app/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='Footer'>Copyright Anish | All rights reserved</div>
+  )
+}
+
+export default Footer
