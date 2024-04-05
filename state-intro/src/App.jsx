501c24930b2a7d7effee263c0cbe9b1a1be46f37
Anish Das <135655078+ragingtitan@users.noreply.github.com>
2024-04-05 16:14:37 +0000

    Create userefhook folder

--- state-intro/src/App.jsx
+++ state-intro/src/App.jsx
@@ -1,11 +1,14 @@
-import { useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  let a=0;
+  useEffect(()=>{
+    console.log(`Re rendering!! and the valuye of a is ${a++}`)
+  },[]);
   return (
     <>
       <div className="count">The count is {count}</div>
